647f1f69f91e02cc77a1410c38a95329263ee1ef
Executed419 <lucker10@interia.pl>
2023-05-19 09:16:44 +0200

    added episodes

--- QLearningAgent.py
+++ QLearningAgent.py
@@ -5,15 +5,18 @@ from MineSweeper import MineSweeper
 
 class QLearningAgent:
     def __init__(self, dimensions):
-        self.game = MineSweeper(dimensions)
+        #self.game = MineSweeper(dimensions)
+        self.game = None
         self.qtable = {}
+        self.episodes = 100
+        self.agent_loop(dimensions)
 
-        self.agent_loop()
-
-    def agent_loop(self):
-        while self.game.game_result == 'safe':
-            self.do_random_move()
-        print("ended due to " + self.game.game_result)
+    def agent_loop(self, dimensions):
+        for episode in range(self.episodes):
+            self.game = MineSweeper(dimensions)
+            while self.game.game_result == 'safe':
+                self.do_random_move()
+            print("ended due to " + self.game.game_result)
 
     def do_random_move(self):
         y, x = random.choice(self.game.get_all_possible_moves())
@@ -26,7 +29,7 @@ class QLearningAgent:
         if neighbours not in self.qtable:
             self.qtable[neighbours] = 0
         else:
-            self.qtable += 1        #reward
+            self.qtable[neighbours] += 1        #reward
 
     def neighbours_to_string(self, neighbours):
         neighbours = list(np.concatenate(neighbours).flat)
