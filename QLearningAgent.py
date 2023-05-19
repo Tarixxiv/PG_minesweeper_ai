e5a35210cbdd75b268e1cb15b69f1ca871d08773
Tarixxiv <r.litwin@interia.pl>
2023-05-19 11:38:53 +0200

    added do_qtable_move()

--- QLearningAgent.py
+++ QLearningAgent.py
@@ -7,7 +7,8 @@ class QLearningAgent:
     def __init__(self, dimensions):
         self.game = None
         self.qtable = {}
-        self.episodes = 100
+        self.episodes = 1000
+        self.random_move_chance = 0.01
         self.agent_loop(dimensions)
         print(self.qtable)
 
@@ -15,7 +16,11 @@ class QLearningAgent:
         for episode in range(self.episodes):
             self.game = MineSweeper(dimensions)
             while self.game.game_result == 'safe':
-                self.do_random_move()
+                if random.random() > self.random_move_chance:
+                    self.do_qtable_move()
+                else:
+                    self.do_random_move()
+
             print("ended due to " + self.game.game_result)
 
     def game_result_to_reward(self):
@@ -26,6 +31,20 @@ class QLearningAgent:
 
     def do_random_move(self):
         y, x = random.choice(self.game.get_all_possible_moves())
+        self.game.action(y, x)
+        self.handle_qtable(y, x)
+
+    def do_qtable_move(self):
+        possible_moves = self.game.get_all_possible_moves()
+        move_scores = []
+        for move in possible_moves:
+            neighbours = self.neighbours_to_string(self.game.get_neighbour_fields(*move))
+            if neighbours in self.qtable:
+                move_scores.append(self.qtable[neighbours])
+            else:
+                move_scores.append(0)
+        best_move_index = move_scores.index(max(move_scores))
+        y, x = possible_moves[best_move_index]
         self.game.action(y, x)
         self.handle_qtable(y, x)
 
